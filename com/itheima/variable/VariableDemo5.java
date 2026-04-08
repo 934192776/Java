6504f243f7bd979a75c5b6c86a37ad5fd8b9e650
jinghui <934192776@qq.com>
2026-04-08 18:54:43 +0800

    update variabledemo5.java

--- com/itheima/variable/VariableDemo5.java
+++ com/itheima/variable/VariableDemo5.java
@@ -1,20 +1,24 @@
 package com.itheima.variable;
 
+import java.util.Scanner;
+
 public class VariableDemo5 {
     public static void main(String[] args) {
         
-    // BMI = 体重 / 身高的平方 
+    // BMI = 体重 / 身高的平方     
 
-    // 1. 定义变量记录我的体重49KG
-    double weight = 49;
+    // 1. 键盘录入体重 kg 
+    Scanner sc = new Scanner(System.in);
+    System.out.println("请输入体重：");
+    double weight = sc.nextDouble();
 
-    // 2. 定义变量记录我的身高1.75M
-    double height = 1.58;
+    // 2. 键盘录入身高 m
+    System.out.println("请输入身高：");
+    double height = sc.nextDouble();
 
     // 3. 计算BMI
     double bmi = weight / (height * height);
     System.out.println(bmi);
-
 }
 
 }
