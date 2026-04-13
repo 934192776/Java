6178e573095cc5924bb3f2187740ec8f13f60bd0
jinghui <934192776@qq.com>
2026-04-13 11:31:35 +0800

    create OperatorDemo5.java

--- com/itheima/operator/OperatorDemo5.java
+++ com/itheima/operator/OperatorDemo5.java
@@ -1,5 +1,25 @@
 package com.itheima.operator;
 
 public class OperatorDemo5 {
+    public static void main(String[] args) {
+        /* 练习2
+        检查下面代码，程序运行时是否会报错，如果会请说明错误原因 */
 
+        short s1 = 100;
+        short s2 = 200;
+   
+        // int + int = int
+        // 修改方案1
+        
+        // 300: 二进制 0000000 00000000 00000001 00101100
+        // 强制转换byte: 00101100（44）
+        byte result1 = (byte) (s1 + s2);
+        System.out.println(result1);
+
+        // 修改方案2
+        int result2 = s1 + s2;
+        System.out.println(result2); //300
+
+   
+    }
 }
