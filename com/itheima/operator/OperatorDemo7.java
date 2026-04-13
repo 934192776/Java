97c48965d4d5bb416fdcbcd03faeb30761614c55
jinghui <934192776@qq.com>
2026-04-13 12:38:57 +0800

    create operatordemo7.java

---
+++ com/itheima/operator/OperatorDemo7.java
@@ -0,0 +1,23 @@
+package com.itheima.operator;
+
+public class OperatorDemo7 {
+    public static void main(String[] args) {
+        /* 
+         = 直接赋值
+         += 后厚赋值
+         -= 减后赋值
+         *= 乘后赋值
+         /= 除后赋值
+         %= 取模后赋值       
+        */
+
+         int a = 10;
+         int b = 20;
+         // a + b ----->a
+         a += b;
+
+         System.out.println(a);
+         System.out.println(b);
+    
+    }
+}
