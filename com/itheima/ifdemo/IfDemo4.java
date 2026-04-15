34e0c49c26173c6897acf88ff558a204f1614b00
jinghui <934192776@qq.com>
2026-04-15 10:43:06 +0800

    create ifdemo4

--- com/itheima/ifdemo/IfDemo4.java
+++ com/itheima/ifdemo/IfDemo4.java
@@ -1,5 +1,18 @@
 package com.itheima.ifdemo;
 
 public class IfDemo4 {
+    public static void main(String[] args) {
+         /*
+         4. 判断布尔类型的变量
+                判断布尔类型的变量，直接把变量写在小括号中即可
+         */
+
+        boolean b = true;
+
+        
+        if(b){
+            System.out.println("为真");
+        }
+    }
 
 }
